697d115059061eff88de5352315296dfde1762fc
atingting <zhangtingting.code@gmail.com>
2018-03-21 23:39:55 +0800

    add spring junit test

--- spring/src/test/TestSpring.java
+++ spring/src/test/TestSpring.java
@@ -1,19 +1,31 @@
 package test;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import pojo.Category;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class TestSpring {
-
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "applicationContext.xml" });
-
-        Category c = (Category) context.getBean("c");
-
+    @Autowired
+    Category c;
+    @Test
+    public void Test(){
         System.out.println(c.getName());
     }
+//    public static void main(String[] args) {
+////        ApplicationContext context = new ClassPathXmlApplicationContext(
+////                new String[] { "applicationContext.xml" });
+////
+////        Category c = (Category) context.getBean("c");
+//
+//        System.out.println(c.getName());
+//    }
 }
 
