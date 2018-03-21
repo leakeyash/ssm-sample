8eb537c84e2cdb0dfe86067e9ea1af9cf0067e47
atingting <zhangtingting.code@gmail.com>
2018-03-21 23:25:04 +0800

    AOP in annotation way

--- spring/src/aspect/ExecutionTimeAspect.java
+++ spring/src/aspect/ExecutionTimeAspect.java
@@ -1,11 +1,20 @@
 package aspect;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
 import java.time.LocalTime;
 
+@Aspect
+@Component
 public class ExecutionTimeAspect {
+    @Before(value = "execution(* service.ProductService.doAnotherService(..)) or execution(* service.ProductService.doSomeService(..))")
     public void startExecuting(){
         System.out.println("start executing:"+ LocalTime.now());
     }
+    @After(value = "execution(* service.ProductService.doAnotherService(..))")
     public void endExecuting(){
         System.out.println("end executing:"+ LocalTime.now());
     }
