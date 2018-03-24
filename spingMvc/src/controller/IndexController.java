755493cf7caf486bfea305797f1134c5078ff974
atingting <zhangtingting.code@gmail.com>
2018-03-24 23:45:55 +0800

    add session

--- spingMvc/src/controller/IndexController.java
+++ spingMvc/src/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @org.springframework.stereotype.Controller
 public class IndexController {
@@ -14,5 +15,26 @@ public class IndexController {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("message", "Hello Spring MVC");
         return modelAndView;
+    }
+    @RequestMapping("/jump")
+    public ModelAndView jump() {
+        ModelAndView mav = new ModelAndView("redirect:/index");
+        return mav;
+    }
+    @RequestMapping("/check")
+    public ModelAndView check(HttpSession session) {
+        Integer i = (Integer) session.getAttribute("count");
+        if (i == null)
+            i = 0;
+        i++;
+        session.setAttribute("count", i);
+        ModelAndView mav = new ModelAndView("check");
+        return mav;
+    }
+    @RequestMapping("/clear")
+    public ModelAndView clear(HttpSession session) {
+        session.setAttribute("count", 0);
+        ModelAndView mav = new ModelAndView("check");
+        return mav;
     }
 }
