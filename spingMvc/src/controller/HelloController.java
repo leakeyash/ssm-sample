267dc6b653104674cd86706160b43fb3b5e5734c
atingting <zhangtingting.code@gmail.com>
2018-03-24 23:12:57 +0800

    change hello to annotation

--- spingMvc/src/controller/HelloController.java
+++ spingMvc/src/controller/HelloController.java
@@ -1,13 +1,15 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloController implements Controller{
-    @Override
+@org.springframework.stereotype.Controller
+public class HelloController{
+    @RequestMapping("/hello")
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView modelAndView=new ModelAndView("hello");
         modelAndView.addObject("test","hahaha");
