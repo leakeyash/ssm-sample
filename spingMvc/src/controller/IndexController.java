a69d91f85ee08978fbf1ce9a78e245a9c0234c71
atingting <zhangtingting.code@gmail.com>
2018-03-24 23:11:00 +0800

    modify to annotation way

--- spingMvc/src/controller/IndexController.java
+++ spingMvc/src/controller/IndexController.java
@@ -1,15 +1,17 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class IndexController implements Controller {
-    @Override
+@org.springframework.stereotype.Controller
+public class IndexController {
+    @RequestMapping("/index")
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
             throws Exception {
-        ModelAndView modelAndView = new ModelAndView("index.jsp");
+        ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("message", "Hello Spring MVC");
         return modelAndView;
     }
