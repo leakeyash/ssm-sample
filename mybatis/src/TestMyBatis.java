68e33c6b5ae9d366ffe83a064f0db1353b54a82a
atingting <zhangtingting.code@gmail.com>
2018-03-31 18:00:48 +0800

    basic CRUD

--- mybatis/src/TestMyBatis.java
+++ mybatis/src/TestMyBatis.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -14,12 +15,34 @@ public class TestMyBatis {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session=sqlSessionFactory.openSession();
+        SqlSession session = sqlSessionFactory.openSession();
 
-        List<Category> cs=session.selectList("listCategory");
+        Category category = new Category();
+        category.setName("新增的Category");
+        session.insert("addCategory", category);
+        listAll(session);
+        Category selectC = session.selectOne("getCategory",category.getId());
+        System.out.println("get one，ID："+selectC.getId() +",Name:"+ selectC.getName());
+        Category updateC = new Category();
+        updateC.setName("修改的Category");
+        updateC.setId(selectC.getId());
+        session.update("updateCategory",updateC);
+        listAll(session);
+        Category c = new Category();
+        c.setId(category.getId());
+        session.delete("deleteCategory",c);
+        listAll(session);
+        List<Category> cs = session.selectList("listCategory");
+        System.out.println(cs.size());
+        session.commit();
+        session.close();
+    }
+
+    private static void listAll(SqlSession session) {
+        List<Category> cs = session.selectList("listCategory");
         for (Category c : cs) {
             System.out.println(c.getName());
-        }
 
+        }
     }
 }
